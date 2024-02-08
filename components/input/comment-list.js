96c25bfd55b427c2b71700c5b6c76eb8a34d7c9f
vivek <chimnanivivek14@gmail.com>
2024-02-08 16:24:01 +0530

    Fetched and Rendered the comments in frontend

--- components/input/comment-list.js
+++ components/input/comment-list.js
@@ -1,21 +1,18 @@
-import classes from './comment-list.module.css';
+import classes from "./comment-list.module.css";
 
-function CommentList() {
+function CommentList({ items }) {
   return (
     <ul className={classes.comments}>
-      {/* Render list of comments - fetched from API */}
-      <li>
-        <p>My comment is amazing!</p>
-        <div>
-          By <address>Maximilian</address>
-        </div>
-      </li>
-      <li>
-        <p>My comment is amazing!</p>
-        <div>
-          By <address>Maximilian</address>
-        </div>
-      </li>
+      {items.map(({ id, name, text }) => {
+        return (
+          <li key={id}>
+            <p>{text}</p>
+            <div>
+              By <address>{name}</address>
+            </div>
+          </li>
+        );
+      })}
     </ul>
   );
 }
