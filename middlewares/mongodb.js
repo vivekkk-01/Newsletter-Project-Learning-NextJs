d3a7c35819c681acc127b76dfbd87fece731b34c
vivek <chimnanivivek14@gmail.com>
2024-02-08 19:09:31 +0530

    connected mongoDB

---
+++ middlewares/mongodb.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const connectDB = (handler) => async (req, res) => {
+  if (mongoose.connections[0].readyState) {
+    return handler(req, res);
+  }
+  await mongoose.connect(process.env.NEXT_PUBLIC_MONGO_URL);
+  return handler(req, res);
+};
+
+export default connectDB;
