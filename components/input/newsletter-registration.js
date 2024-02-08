d9adc15f934b2c67e3aae958116f7fd6d3415b7e
vivek <chimnanivivek14@gmail.com>
2024-02-08 13:12:48 +0530

    Fetched and validate user input in frontend

--- components/input/newsletter-registration.js
+++ components/input/newsletter-registration.js
@@ -1,11 +1,18 @@
-import classes from './newsletter-registration.module.css';
+import { useState } from "react";
+import classes from "./newsletter-registration.module.css";
 
 function NewsletterRegistration() {
+  const [email, setEmail] = useState("");
+  const [emailError, setEmailError] = useState("");
   function registrationHandler(event) {
     event.preventDefault();
-
-    // fetch user input (state or refs)
-    // optional: validate input
+    const isEmailValid =
+      /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(email) &&
+      email.trim() !== "";
+    if (!isEmailValid) {
+      setEmailError("Enter a valid Email Address.");
+      return;
+    }
     // send valid data to API
   }
 
@@ -13,12 +20,29 @@ function NewsletterRegistration() {
     <section className={classes.newsletter}>
       <h2>Sign up to stay updated!</h2>
       <form onSubmit={registrationHandler}>
+        {emailError && (
+          <p
+            style={{
+              textAlign: "center",
+              color: "red",
+              fontSize: "14px",
+              margin: ".5rem 0",
+            }}
+          >
+            {emailError}
+          </p>
+        )}
         <div className={classes.control}>
           <input
-            type='email'
-            id='email'
-            placeholder='Your email'
-            aria-label='Your email'
+            type="email"
+            id="email"
+            placeholder="Your email"
+            aria-label="Your email"
+            value={email}
+            onChange={({ target }) => {
+              setEmailError("")
+              setEmail(target.value)
+            }}
           />
           <button>Register</button>
         </div>
